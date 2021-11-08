b672dbfcd5a42e1b774c955c3ce1ceb9f7f97f33
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-08 16:50:26 -0300

    :hammer: Initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+
+import { Routers } from "./routes";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routers />
+            <GlobalStyle />
+        </BrowserRouter>
+    );
 }
 
 export default App;
