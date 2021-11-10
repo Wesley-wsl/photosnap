c6dcd43c057e5e4d19e4658e25c2dc7a88daffc0
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-10 15:16:56 -0300

    :hammer: feat: header and footer added in all app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import { Routers } from "./routes";
 import { GlobalStyle } from "./styles/GlobalStyle";
 
-function App() {
+const App: React.FC = () => {
     return (
         <BrowserRouter>
+            <Header />
             <Routers />
+            <Footer />
             <GlobalStyle />
         </BrowserRouter>
     );
-}
+};
 
 export default App;
