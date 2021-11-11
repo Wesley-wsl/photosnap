c31579fc23c7c90e348f2006640e5a6bc17ae4ec
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-11 18:02:52 -0300

    :wrench: feat: new routes added
    routes for stories, features and pricing added in application.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,18 @@
 import { Switch, Route } from "react-router-dom";
 
+import Features from "../pages/Features";
 import Home from "../pages/Home";
+import Pricing from "../pages/Pricing";
+import Stories from "../pages/Stories";
 
 export const Routers: React.FC = () => {
     return (
         <>
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/stories" exact component={Stories} />
+                <Route path="/features" exact component={Features} />
+                <Route path="/pricing" exact component={Pricing} />
             </Switch>
         </>
     );
