968f3f811214efc1ffbfa1808345c67b6a5c3bd0
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-13 14:32:44 -0300

    :wrench: feat: stories data created

---
+++ src/services/data.ts
@@ -0,0 +1,101 @@
+import daysVoyage from "../assets/stories/desktop/18-days-voyage.jpg";
+import architecturals from "../assets/stories/desktop/architecturals.jpg";
+import behindTheWaves from "../assets/stories/desktop/behind-the-waves.jpg";
+import calmWaters from "../assets/stories/desktop/calm-waters.jpg";
+import cityScapes from "../assets/stories/desktop/cityscapes.jpg";
+import darkForest from "../assets/stories/desktop/dark-forest.jpg";
+import kingOnAfrica from "../assets/stories/desktop/king-on-africa.jpg";
+import landOfDreams from "../assets/stories/desktop/land-of-dreams.jpg";
+import milkyWay from "../assets/stories/desktop/milky-way.jpg";
+import mountains from "../assets/stories/desktop/mountains.jpg";
+import rageOfTheSea from "../assets/stories/desktop/rage-of-the-sea.jpg";
+import runningFree from "../assets/stories/desktop/running-free.jpg";
+import somWarpet from "../assets/stories/desktop/somwarpet.jpg";
+import tripToNowhere from "../assets/stories/desktop/trip-to-nowhere.jpg";
+import unforeseenCorners from "../assets/stories/desktop/unforeseen-corners.jpg";
+import worldTour from "../assets/stories/desktop/world-tour.jpg";
+
+export default {
+    stories: [
+        {
+            title: "The Mountain",
+            author: "John Apppleseed",
+            background: mountains,
+        },
+        {
+            title: "Sunset Cityscapes",
+            author: "Benjamin Crud",
+            background: cityScapes,
+        },
+        {
+            title: "18 Days Voyage",
+            author: "Alexei Borodin",
+            background: daysVoyage,
+        },
+        {
+            title: "Architecturals",
+            author: "John Apppleseed",
+            background: architecturals,
+        },
+        {
+            title: "World Tour 2019",
+            author: "Timothy Wagner",
+            background: worldTour,
+        },
+        {
+            title: "Unforeseen Corner",
+            author: "William Malcom",
+            background: unforeseenCorners,
+        },
+        {
+            title: "King on África: Part II",
+            author: "Timothy Wagner",
+            background: kingOnAfrica,
+        },
+        {
+            title: "The Trip to Nowhere",
+            author: "Felicia Rourke",
+            background: tripToNowhere,
+        },
+        {
+            title: "Rage of The Sea",
+            author: "Mohammed Abdul",
+            background: rageOfTheSea,
+        },
+        {
+            title: "Running Free",
+            author: "Michele",
+            background: runningFree,
+        },
+        {
+            title: "Behind the Waves",
+            author: "Felicia Rourke",
+            background: behindTheWaves,
+        },
+        {
+            title: "Calm Waters",
+            author: "Samantha Brooke",
+            background: calmWaters,
+        },
+        {
+            title: "The Milky Way",
+            author: "Benjamin Cruz",
+            background: milkyWay,
+        },
+        {
+            title: "Night at The Dark Forest",
+            author: "Mohammed Abdul",
+            background: darkForest,
+        },
+        {
+            title: "Somwapet's Beauty",
+            author: "Michelle",
+            background: somWarpet,
+        },
+        {
+            title: "Land of Dreams",
+            author: "William Malcolm",
+            background: landOfDreams,
+        },
+    ],
+};
