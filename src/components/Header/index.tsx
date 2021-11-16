9e00b59c26d493e4dfe7b2d3225fc593212a6a9c
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-15 21:50:43 -0300

    :wrench: feat: header mobile created

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import logo from "../../assets/shared/desktop/logo.svg";
-import { Container } from "./styles";
+import { Container, MenuMobile } from "./styles";
 
 const Header: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <Container>
             <div>
@@ -25,6 +29,35 @@ const Header: React.FC = () => {
                     </li>
                 </ul>
             </nav>
+
+            <MenuMobile>
+                <div
+                    onClick={() => setIsOpen(!isOpen)}
+                    className={`openNav ${
+                        isOpen ? "closeIcon" : "defaultIcon"
+                    }`}
+                >
+                    <div />
+                    <div />
+                </div>
+                <nav className={isOpen ? "open" : "exit"}>
+                    <ul>
+                        <li>
+                            <Link to="/stories">Stories</Link>
+                        </li>
+                        <li>
+                            <Link to="/features">Features</Link>
+                        </li>
+                        <li>
+                            <Link to="/pricing">Pricing</Link>
+                        </li>
+                        <li>
+                            <hr />
+                            <button type="button">Get an invite</button>
+                        </li>
+                    </ul>
+                </nav>
+            </MenuMobile>
 
             <button type="button">Get an invite</button>
         </Container>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,11 +5,21 @@ export const Container = styled.header`
     align-items: center;
     justify-content: space-around;
     padding: 1.1rem;
+    z-index: 99;
 
-    div {
+    div > div {
         cursor: pointer;
     }
+    @media (max-width: 768px) {
+        justify-content: space-between;
+        nav {
+            display: none;
+        }
 
+        button {
+            display: none;
+        }
+    }
     nav > ul {
         display: flex;
         gap: 2rem;
@@ -44,6 +54,98 @@ export const Container = styled.header`
             background-color: #fff;
             color: #000;
             border: 0.1rem solid #000;
+        }
+    }
+`;
+
+export const MenuMobile = styled.div`
+    cursor: default;
+    transition: all 0.2s ease-in-out;
+    @media (min-width: 768px) {
+        display: none;
+    }
+    @media (max-width: 768px) {
+        .openNav {
+            cursor: pointer;
+            padding: 0.3rem;
+            z-index: 99;
+            div {
+                height: 1px;
+                width: 2rem;
+                background-color: #000;
+            }
+
+            div:nth-child(1) {
+                margin-bottom: 0.4rem;
+            }
+        }
+    }
+    nav {
+        transition: height 0.3s linear;
+        height: 192px;
+        overflow: hidden;
+        display: flex;
+        justify-content: center;
+        top: 3rem;
+        left: 0;
+        right: 0;
+        position: absolute;
+        background-color: #fff;
+        height: 12rem;
+        z-index: 2;
+        width: 100%;
+
+        ul {
+            flex-direction: column;
+            gap: 1.4rem;
+            align-items: center;
+            transition: height 0.3s linear;
+            height: 192px;
+
+            li > hr {
+                display: block;
+                background-color: #111;
+                opacity: 0.4;
+                margin-bottom: 1rem;
+                height: 0.1rem;
+                width: 200%;
+                align-items: center;
+                margin-left: -2rem;
+            }
+
+            li > button {
+                display: block;
+                width: 200%;
+                margin-left: -2rem;
+            }
+        }
+    }
+
+    .exit {
+        transition: height 0.3s linear;
+        height: 0px;
+    }
+
+    .closeIcon {
+        div:nth-child(1) {
+            transition: transform 0.3s linear;
+            transform: rotate(45deg);
+        }
+        div:nth-child(2) {
+            transition: transform 0.3s linear;
+            transform: rotate(135deg);
+            margin-top: -7.5px;
+        }
+    }
+    .defaultIcon {
+        div:nth-child(1) {
+            transition: transform 0.3s linear;
+            transform: rotate(180deg);
+        }
+        div:nth-child(2) {
+            transition: transform 0.3s linear;
+            transform: rotate(0);
+            margin-top: 0px;
         }
     }
 `;
